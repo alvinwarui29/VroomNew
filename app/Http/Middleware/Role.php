265b7bdf57f28062e8234557623b34f824df7832
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-11-14 02:25:55 +0300

    asset set up

---
+++ app/Http/Middleware/Role.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Role
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next,$role): Response
+    {
+        $redirectRoute = 'dashboard'; // Default redirect route
+
+    switch ($role) {
+        case 'user':
+            $redirectRoute = 'user.dashboard';
+            break;
+        case 'agency':
+            $redirectRoute = 'agency.dashboard';
+            break;
+        case 'admin':
+            $redirectRoute = 'admin.dashboard';
+            break;
+        // Add more cases as needed for other roles
+
+        default:
+            // Handle any unexpected roles or set a default route
+            break;
+    }
+
+    if ($request->user()->role !== $role) {
+        return redirect($redirectRoute);
+    }
+    return $next($request);
+    }
+}
